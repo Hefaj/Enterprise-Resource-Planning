c7aedc13438109db826b22a2ce219517a0746370
Hefaj <hefajstore@gmail.com>
2026-03-09 20:38:45 +0100

    feat(dashboard): enhance form handling and add save functionality

--- frontend/apps/client/src/app/_components/dashboard/dashboard.component.ts
+++ frontend/apps/client/src/app/_components/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CheckboxModule } from 'primeng/checkbox';
 import { InputTextModule } from 'primeng/inputtext';
 import { EInputTextBuilder, EInputTextComponent, ETwoSectionLayoutComponent } from '@erp/shared/ui';
+import { EButtonBuilder, EButtonComponent, EButtonSave } from '@erp/shared-ui';
+import { timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,9 +14,10 @@ import { EInputTextBuilder, EInputTextComponent, ETwoSectionLayoutComponent } fr
     CommonModule,
     CheckboxModule,
     InputTextModule,
-    ButtonModule,
+    EButtonComponent,
     EInputTextComponent,
     ETwoSectionLayoutComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -23,8 +25,10 @@ import { EInputTextBuilder, EInputTextComponent, ETwoSectionLayoutComponent } fr
 export class DashboardComponent {
   public myDynamicForm = new FormGroup({
     email: new FormControl('jan@example.com', [Validators.required, Validators.email]),
-    age: new FormControl(30, [Validators.required, Validators.minLength(5)]),
+    age: new FormControl(null, [Validators.required, Validators.minLength(5)]),
   });
+
+  public isLoading = signal(false);
 
   public control = this.myDynamicForm.controls.email;
 
@@ -46,4 +50,22 @@ export class DashboardComponent {
       min: 'min',
     });
   });
+
+  public saveBtn = EButtonBuilder.create(EButtonSave);
+
+  public save(): void {
+    if (this.myDynamicForm.invalid) {
+      this.myDynamicForm.markAllAsDirty();
+      return;
+    }
+    const formData = this.myDynamicForm.value;
+    this.myDynamicForm.disable();
+    this.isLoading.set(true);
+    setTimeout(() => {
+      this.myDynamicForm.reset();
+      console.log(formData);
+      this.myDynamicForm.enable();
+      this.isLoading.set(false);
+    }, 1000);
+  }
 }

00909808badfe2460b0dfde3300721a1bb8a8751
Hefaj <hefajstore@gmail.com>
2026-05-04 18:54:46 +0200

    feat: initialize index.ts for catalog UI module

--- frontend/libs/modules/catalog/ui/src/lib/catalog-ui/catalog-ui.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'erp',
-  imports: [],
-  templateUrl: './catalog-ui.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CatalogUi {}
